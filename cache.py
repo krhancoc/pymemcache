252a00a8b41408d35cb3063462095607b5e53930
Kenneth R Hancock <krhancoc@uwaterloo.ca>
2018-10-14 15:37:32 -0400

    dyanmic cache disply UI

--- cache.py
+++ cache.py
@@ -1,9 +1,13 @@
+import os
 import sys
 import collections
-import os
 import pickle
+import time
+import curses
 
+from abc import ABC, abstractmethod
 from enum import Enum
+from texttable import Texttable
 
 
 class CacheI(Enum):
@@ -20,24 +24,50 @@ class CacheManager:
         self._registered.append(cache)
 
     def display(self):
-        for c in self._registered:
-            print(c.info())
+        """ Display will print a table of all the caches in real time
+
+        Note : Run this in a new thread!
+        """
+        def helper(window):
+            window.nodelay(True)
+            while (True):
+                window.clear()
+                table = Texttable()
+                table.set_cols_align(["l", "l", "r", "r", "l", "l"])
+                rows = [c.info() for c in self._registered]
+                final = [["Location", "Cache Type", "Entries",
+                        "Size (bytes)", "Hit (%)", "Adds causing resize (%)"]] + rows
+                table.add_rows(final)
+                window.addstr(table.draw())
+                window.refresh()
+                c = window.getch()
+                if c == ord('q'):
+                    break
+                time.sleep(0.5)
+
+        curses.wrapper(helper)
 
 
 GlobalCacheManager = CacheManager()
 
 
-class Cache:
+class Cache(ABC):
 
     def __init__(self, size, style):
         self._size = size
         self._style = style
 
+    @abstractmethod
     def fetch(self, key):
-         raise NotImplementedError("missing implementation")
+        raise NotImplementedError("missing implementation")
 
+    @abstractmethod
     def add(self, key, value):
-         raise NotImplementedError("missing implementation")
+        raise NotImplementedError("missing implementation")
+
+    @abstractmethod
+    def info(self):
+        raise NotImplementedError("missing implementation")
 
 
 class LRU(Cache):
@@ -71,7 +101,7 @@ class LRU(Cache):
         self._misses = 0
         self._resizes = 0
 
-
+    # @profile
     def fetch(self, key):
         """ Fetches a value from the cache, returns None when key is not there"
 
@@ -91,18 +121,31 @@ class LRU(Cache):
             self._misses += 1
             return None
 
+    # @profile
     def pop(self):
+        """ Pops the least recently used element from the cache"""
         item = None
         if len(self._cache) > 0:
             item = self._cache.popitem(last=False)
         return item
 
+    # @profile
     def add(self, key, value):
+        """ Adds a key and value pair to the cache
+
+        Note : This sometimes forces a resize of the cache, this occurs
+        rarely but its around to make sure we enforce the size restriction
+        set by the user
+
+
+        Args:
+            key (any) : The key of the cache entry
+            value (any): The value to be inserted at they key
+        """
         if self._check_filled():
             self.pop()
         self._cache[key] = value
-        # AS WE KNOW MAX_SIZE NOW AND LETS MAINTAIN IT
-        if (len(self) > self._size):
+        if (self.bsize() > self._size):
             self._resizes += 1
             self._max_size = len(self._cache) - 2
             new_cache = collections.OrderedDict()
@@ -112,9 +155,31 @@ class LRU(Cache):
             self._cache = new_cache
             self.add(key, value)
 
+    def bsize(self):
+        """ Byte size of our cache object """
+        sizeof = sys.getsizeof
+        size = sizeof(self._hits)
+        size += sizeof(self._size)
+        size += sizeof(self._hits)
+        size += sizeof(self._style)
+        size += sizeof(self._misses)
+        size += sizeof(self._resizes)
+        size += sizeof(self._cache)
+
+        return size
+
+    def info(self):
+        hit_p = 0
+        resize_p = 0
+        denom = (self._hits + self._misses)
+        if denom > 0:
+            hit_p = (self._hits / denom) * 100
+            resize_p = (self._resizes / self._misses) * 100
+        return [self._style, len(self), self.bsize(), hit_p, resize_p]
+
     def __len__(self):
 
-        return sys.getsizeof(self._cache)
+        return len(self._cache)
 
     def __str__(self):
         hit_p = 0
@@ -123,22 +188,26 @@ class LRU(Cache):
         if denom > 0:
             hit_p = (self._hits / denom) * 100
             resize_p = (self._resizes / self._misses) * 100
-        return "%s - %d bytes, %.2f%% hit, %.2f%% resizes" % \
-            (self._style, len(self), hit_p, resize_p)
+        return "%s - %d entries, %d bytes, %.2f%% hit, %.2f%% adds cause resize" % \
+            (self._style, len(self), self.bsize(), hit_p, resize_p)
 
     def print(self):
+        """ Prints all cache entries in the cache"""
         for x, y in self._cache.items():
             print("Key %s : %s" % (str(x), str(y)))
 
     def _check_filled(self):
         if self._max_size > 0:
-            return self._max_size <= len(self._cache)
+            return self._max_size <= len(self)
 
-        return len(self) >= self._size
+        return self.bsize() >= self._size
 
 
 def memcache(size, style=CacheI.LRU):
+    """Decorator caching object for functions
 
+
+    """
     MIN_SIZE = 2048
 
     assert size >= MIN_SIZE
@@ -150,6 +219,7 @@ def memcache(size, style=CacheI.LRU):
             self.style = style
             GlobalCacheManager.register(self)
 
+        # @profile
         def __call__(self, *args):
             value = pickle.dumps(args)
             response = self.cache.fetch(value)
@@ -172,8 +242,13 @@ def memcache(size, style=CacheI.LRU):
                 self.cache = None
 
         def info(self):
+            info = []
+            info.append(
+                "%s:%s" %
+                (os.path.basename(__file__),
+                 self.func.__name__))
+            info.extend(self.cache.info())
 
-            return "%s:%s:%s" % (os.path.basename(__file__),
-                                 self.func.__name__, str(self.cache))
+            return info
 
     return cacheobject
--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-import cache
+from cache import memcache, GlobalCacheManager
+from threading import Thread
 import logging
 import sys
+import random
 
 sys.setrecursionlimit(10000)
-
 
 
 KB = 1024
@@ -12,7 +13,7 @@ MB = 1024 * KB
 logging.basicConfig(level=logging.INFO)
 
 
-@cache.memcache(2 * KB)
+@memcache(2 * KB)
 def fib(n):
     if n <= 1:
 
@@ -21,7 +22,7 @@ def fib(n):
     return fib(n-1) + fib(n-2)
 
 
-@cache.memcache(50 * KB)
+@memcache(50 * KB)
 def fib2(n):
     if n <= 1:
 
@@ -30,36 +31,49 @@ def fib2(n):
     return fib2(n-1) + fib2(n-2)
 
 
-@cache.memcache(2 * KB)
+@memcache(2 * KB)
 def multiple_param(x, y):
     return x + y
 
-@cache.memcache(2 * KB)
+
+@memcache(2 * KB)
 def list_check(y):
     return y
 
-@cache.memcache(2 * KB)
+
+@memcache(2 * KB)
 def string_check(y):
     return y
 
-@cache.memcache(2 * KB)
+
+@memcache(2 * KB)
 def object_check(y):
     return y
+
 
 dictionary = {}
 for x in range(100000):
     dictionary[x] = x
 
-gcm = cache.GlobalCacheManager
-gcm.display()
+gcm = GlobalCacheManager
+t = Thread(target = gcm.display)
+t.start()
 fib2(1000)
 fib2(2000)
-multiple_param(1, 2)
-multiple_param(1, 2)
-list_check([1,2,3,4])
-list_check([1,2,3,4])
+for x in range(10000):
+    fib(500)
+
+for x in range(100000):
+    k = random.randint(0, 11)
+    j = random.randint(0, 11)
+    multiple_param(k, j)
+
+for x in range(100000):
+    l = [random.randint(0, 11), random.randint(0, 11)]
+    list_check(l)
+
 string_check("hello")
 string_check("hello")
 object_check(dictionary)
 object_check(dictionary)
-gcm.display()
+t.join()
