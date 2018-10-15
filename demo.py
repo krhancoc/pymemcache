a16855143a1b7d7d2f04279a83ad910d4f3e6d13
Kenneth R Hancock <krhancoc@uwaterloo.ca>
2018-10-15 10:56:17 -0400

    demo

--- demo.py
+++ demo.py
@@ -12,33 +12,15 @@ MB = 1024 * KB
 
 logging.basicConfig(level=logging.INFO)
 
-@memcache(5 * KB, CacheI.CLOCK)
-def fib2(n):
-    if n <= 1:
-
-        return n
-
-    return fib2(n-1) + fib2(n-2)
-
-
 def cache_test(t):
 
-    @memcache(2 * KB, t)
+    @memcache(50 * KB, t)
     def fib(n):
         if n <= 1:
 
             return n
 
         return fib(n-1) + fib(n-2)
-
-
-    @memcache(50 * KB, t)
-    def fib2(n):
-        if n <= 1:
-
-            return n
-
-        return fib2(n-1) + fib2(n-2)
 
 
     @memcache(2 * KB, t)
@@ -69,8 +51,8 @@ def cache_test(t):
     for x in range(100000):
         dictionary[x] = x
 
-    fib2(1000)
-    fib2(2000)
+    fib(1000)
+    fib(2000)
     for x in range(100, 100000, 1000):
         for _ in range(100):
 
@@ -80,9 +62,10 @@ def cache_test(t):
                 local_with_some_random(t)
 
             #random
-            for _ in range(3):
+            for _ in range(4):
                 r = random.randint(1000000, 2000000)
                 local_with_some_random(r)
+
 
 
 
@@ -105,9 +88,6 @@ def cache_test(t):
 gcm = GlobalCacheManager
 t = Thread(target = gcm.display, args = (True,))
 t.start()
-
-for x in range(3000):
-    fib2(x)
 cache_test(CacheI.LRU)
 cache_test(CacheI.CLOCK)
 cache_test(CacheI.FIFO)
